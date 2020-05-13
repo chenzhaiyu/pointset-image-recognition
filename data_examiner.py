5fce961817d4c1ff315ae8d0d5f17a69414dadb2
Zhaiyu <contact@chenzhaiyu.com>
2020-05-13 21:46:17 +0200

    Initial commit with additional support on mnist and pascal datasets

---
+++ data_examiner.py
@@ -0,0 +1,5 @@
+import numpy as np
+data_s3dis = np.load("/media/zhaiyu/7CF2DC06F2DBC296/Datasets/stanford_indoor3d/Area_1_conferenceRoom_1.npy")
+data_pascal = np.load("/media/zhaiyu/7CF2DC06F2DBC296/Github/Pointnet_Pointnet2_pytorch/data/pascal_point_cloud/Area_1_2007_000032.npy")
+
+print("ok")
