c00c846ea806b5b89afc85468272f4605ad01d2a
Zhaiyu Chen <zhaiyu.chen@outlook.com>
2020-06-21 21:35:45 +0200

    Consistency score calculation for PointCNN

--- shift-invariance-exp/pointcnn_exp.py
+++ shift-invariance-exp/pointcnn_exp.py
@@ -6,6 +6,7 @@ import sys
 import os
 import numpy as np
 import glob
+import random
 
 
 def pc_normalize(pc):
@@ -15,35 +16,50 @@ def pc_normalize(pc):
     pc = pc / m
     return pc
 
+
+dataset_dir = "./translation_samples/shifted_point_cloud/"
 sys.path.append(os.path.join(os.path.abspath(os.getcwd()), '../models'))
 MODEL = importlib.import_module('pointcnn_cls')
 classifier = MODEL.get_model(10, normal_channel=True).cuda()
-checkpoint = torch.load('/media/zhaiyu/7CF2DC06F2DBC296/Github/pointnet-image-parsing/log/cls/pointcnn_cifar/checkpoints/best_model.pth')
+checkpoint = torch.load('/media/zhaiyu/7CF2DC06F2DBC296/Github/pointnet-image-parsing/log/cls/pointcnn_cifar'
+                        '/checkpoints/best_model.pth')
 classifier.load_state_dict(checkpoint['model_state_dict'])
 
-images = []
-filenames = []
+num_total = 0
+num_correct = 0
 
-for f in glob.iglob("./point_cloud_data/*jet*"):
-    images.append((np.loadtxt(f, delimiter=',', dtype=np.float32)))
-    filenames.append(f)
+# for filename in glob.iglob(os.path.join(dataset_dir, "*biplane_s_000764.txt")):
+for filename in glob.iglob(os.path.join(dataset_dir, "*.txt")):
 
+    name_base = filename.split('/')[-1][6:]
+    name_list = [dataset_dir + 'shift' + str(i) + name_base for i in range(8)]
 
-for image, filename in zip(images, filenames):
-    # normalise rgb
-    image[:, 3:6] = image[:, 3:6] / 255.0
-    image[:, 0:3] = pc_normalize(image[:, 0:3])
+    predicted_classes = [None, None]
 
-    image = torch.from_numpy(image).cuda()
+    for i, name in enumerate(random.sample(name_list, 2)):
 
-    # todo: 256 vs 512
-    batch = torch.from_numpy(np.zeros(512, dtype=np.int)).cuda()
+        label = int(name.split("_")[4])
+        points = np.loadtxt(name, delimiter=",", dtype=np.float32)
+        points[:, 3:6] = points[:, 3:6] / 255.0
+        points[:, 0:3] = pc_normalize(points[:, 0:3]).astype(np.float32)
 
-    pred, _ = classifier(image, batch)
+        points = torch.from_numpy(points).cuda()
 
-    m = torch.nn.Softmax(dim=1)
-    output = m(pred)
+        batch = torch.from_numpy(np.zeros(512, dtype=np.int)).cuda()
 
-    print(filename)
-    print('prediction: class ' + str(np.argmax(output.cpu().detach().numpy(), axis=1)) + ' with ' + 'probability ' + str(np.max(output.cpu().detach().numpy())))
-    print("\n")
+        prediction, _ = classifier(points, batch)
+
+        m = torch.nn.Softmax(dim=1)
+        output = m(prediction)
+        predicted_class = np.argmax(output.cpu().detach().numpy(), axis=1)
+        predicted_classes[i] = predicted_class
+        predicted_prob = np.max(output.cpu().detach().numpy())
+
+        print("filename: " + name)
+        # print("label: " + str(label))
+        print('prediction: class ' + str(predicted_class.item()) + ' with ' + 'probability ' + str(predicted_prob))
+
+    num_total = num_total + 1
+    num_correct = num_correct + (predicted_classes[0] == predicted_classes[1]).item()
+
+    print("consistency: " + str(num_correct / num_total) + " %\n")
